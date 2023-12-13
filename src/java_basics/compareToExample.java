6d2a95e7260ac56915a22f94a02dbe092507fa73
munugala26 <munugala26@gmail.com>
2023-12-13 16:44:12 -0500

    Commit 2 for Release 1.0

--- src/java_basics/compareToExample.java
+++ src/java_basics/compareToExample.java
@@ -10,4 +10,5 @@ public class compareToExample {
         System.out.println(s1.compareTo(s2)); // 0 because both are equal
         System.out.println(s1.compareTo(s3)); //-1 because "l" is only one time lower than "m"
         System.out.println(s1.compareTo(s4)); // 2 because "h" is 2 times greater than "f"
+        System.out.println(s1.compareTo(s4)); // 2 because "h" is 2 times greater than "f"
     }}
